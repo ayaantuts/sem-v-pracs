e2cdb8a61438d784fb33ee4c80001fa5a3387d1c
Ayaan Shaikh <ayaanmaths04@gmail.com>
2024-08-14 15:58:17 +0530

    chore: Refactor Character_Count.c for improved readability and memory management

--- CN/Experiment 2/Character_Count.c
+++ CN/Experiment 2/Character_Count.c
@@ -1,18 +1,23 @@
 #include<stdio.h>
+#include<malloc.h>
 #include<string.h>
 char frames[10][50], final[50], *len;
 int no_of_frames;
 int main() {
     int i, j = 0;
     printf("Enter the number of frames: ");
-    scanf("%d\n", &no_of_frames);
+    scanf("%d", &no_of_frames);
     for (i = 0; i < no_of_frames; i++) {
-        gets(frames[i]);
+        scanf("%s", frames[i]);
     }
     for (i = 0; i < no_of_frames; i++) {
-        final[j++] = '0' + strlen(frames[i]);
+        // Convert the length of the frame to a stream of characters and append it to the frame
+        len = (char *)malloc(strlen(frames[i]) + 1);
+        sprintf(len, "%lu", strlen(frames[i]));
+        strcat(final, len);
         strcat(final, frames[i]);
         j += strlen(frames[i]);
+        free(len);
     }
     puts(final);
 }
