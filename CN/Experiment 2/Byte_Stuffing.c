a0ae490319ffac172273c5788813aeb365fdc13f
Ayaan Shaikh <ayaanmaths04@gmail.com>
2024-08-15 22:28:48 +0530

    refactor: Improve byte stuffing logic for better performance and readability

--- CN/Experiment 2/Byte_Stuffing.c
+++ CN/Experiment 2/Byte_Stuffing.c
@@ -1,27 +1,34 @@
 #include <stdio.h>
-#include <string.h>
 
-char string[50], final[50], start, end, flag;
+char string[50], final[300], start, end, flag;
 
 int main() {
     int i = 0, j = 0;
     printf("Enter the message: ");
-    gets(string);
+    scanf("%s", string);
     printf("\nEnter the Start Flag: ");
-    scanf("%c", &start);
+    // The \n%c is because, to end previous output, newline character is entered
+    // (enter key is pressed) hence, We need to consume(read/scan) it
+    scanf("\n%c", &start);
     printf("\nEnter the End Flag: ");
     scanf("\n%c", &end);
     printf("\nEnter the Escape Flag: ");
     scanf("\n%c", &flag);
+    // Append the start character
     final[j++] = start;
     while (string[i] != '\0') {
+        // Check if the string contains either of start, end or flag character
         if (string[i] == start || string[i] == end || string[i] == flag) {
+            // If there is any, pre-append by flag character
             final[j++] = flag;
         }
+        // Append the character in the final string
         final[j++] = string[i];
         i++;
     }
+    // Append the ending character, and subsequently, terminate the string
     final[j++] = end;
     final[j] = '\0';
-    puts(final);
+    printf("%s\n", final);
+    return 0;
 }
