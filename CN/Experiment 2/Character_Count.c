3dcb1a42913d3ae9b213226871db2617291a3fcb
Ayaan Shaikh <ayaanmaths04@gmail.com>
2024-08-19 22:25:34 +0530

    refactor: Improve memory management and logic in Character_Count.c

--- CN/Experiment 2/Character_Count.c
+++ CN/Experiment 2/Character_Count.c
@@ -1,8 +1,7 @@
 #include<stdio.h>
-#include<malloc.h>
 #include<string.h>
 
-char frames[4][50], final[300], *len;
+char frames[4][50], final[300], len[20];
 int no_of_frames;
 
 int main() {
@@ -16,15 +15,12 @@ int main() {
     }
     for (i = 0; i < no_of_frames; i++) {
         // Convert the length of the frame to a stream of characters and append it to the final string
-        len = (char *)malloc(strlen(frames[i]) + 1);
         // ssprintf will store the length as stream of characters in the char* len
-        sprintf(len, "%lu", strlen(frames[i]));
+        sprintf(len, "%lu", strlen(frames[i]) + 1);
         // Concatenate the len string to the final string
         strcat(final, len);
         // Finally, append the frame
         strcat(final, frames[i]);
-        // Free len to improve memory
-        free(len);
     }
     printf("%s\n", final);
     return 0;
