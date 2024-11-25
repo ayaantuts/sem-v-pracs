9d6a300ab74ac473117e4ddf549c1c76f22e2f72
Ayaan Shaikh <ayaanmaths04@gmail.com>
2024-11-25 14:36:35 +0530

    Refactor CRC calculation to simplify remainder logic and improve error detection

--- CN/Experiment 3/CRC.py
+++ CN/Experiment 3/CRC.py
@@ -16,27 +16,13 @@ def xor(a, b):
 	return '1'
 
 def calcRemainder(data, generator):
-	# curr = the part of data that is currently being divided
-	curr = []
-	pointer = 0
-	while pointer < len(generator):
-		curr.append(data[pointer])
-		pointer += 1
-
-	while pointer < len(data):
-		if curr[0] == '1':
+	while len(data) >= len(generator):
+		if data[0] == '1':
 			for i in range(len(generator)):
-				curr[i] = xor(curr[i], generator[i])
-		curr.pop(0)
-		curr.append(data[pointer])
-		pointer += 1
-	# This if is for the last iteration, as we don't want to append anything further
-	# If we keep it in the loop, it will give Index out of range error
-	if curr[0] == '1':
-		for i in range(len(generator)):
-			curr[i] = xor(curr[i], generator[i])
-	curr.pop(0)
-	return curr
+				data[i] = xor(data[i], generator[i])
+		while data and data[0] == '0':
+			data.pop(0)
+	return data
 
 remainder = calcRemainder(data, generator)
 crc = original_data + ''.join(remainder)
@@ -47,18 +33,13 @@ print()
 # Receiver side:
 received_data = input('Enter the received data: ')
 received_generator = input('Enter the generator polynomial: ')
+original_dataR = received_data[:len(received_data) - len(received_generator) + 1]
 received_data = list(received_data)
-generatorR = list(received_generator)
-remainderR = calcRemainder(received_data, generatorR)
-error = False
-for r in remainderR:
-	if r == '1':
-		error = True
-		break
+received_generator = list(received_generator)
+remainderR = calcRemainder(received_data, received_generator)
 
-if error:
+if '1' in remainderR:
 	print('Error detected')
 else:
 	print('No error detected')
-	original_dataR = received_data[:len(received_data) - len(generatorR) + 1]
-	print('Original data:', ''.join(original_dataR))
+	print('Original data:', original_dataR)
